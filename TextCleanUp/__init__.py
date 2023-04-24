75cbff1ecc974cca7096d2c83250a5ae45e625f3
Suraj Yadav <qwertypad7612@gmail.com>
2023-04-24 22:00:26 +0530

    Updating docstring

--- TextCleanUp/__init__.py
+++ TextCleanUp/__init__.py
@@ -2,22 +2,99 @@ from TextCleanUp import utils
 
 __version__ = '0.0.2'
 
-def get_word_count(text):
+def get_word_count(text: str) -> int: 
+    """
+    This function takes a string of text as input and returns the word count in the input string.
+    
+    Parameters:
+    - text (str): The input string whose word count is to be computed.
+    
+    Returns:
+    - int: The number of words in the input string.
+    
+    Example:
+    >>> get_word_count("Hello, this is a sample sentence.")
+    6
+    """
     return utils._get_word_count(text)
 
-def get_char_count(text):
+def get_char_count(text: str) -> int:
+    """
+    This function takes a string as input and returns the count of non-whitespace characters in the string.
+
+    Parameters:
+    - text (str): The input string for which non-whitespace character count is to be calculated. 
+
+    Returns:
+    - int: The count of non-whitespace characters in the input string.
+
+    Example:
+    >>> get_char_count('The quick brown fox jumps over the lazy dog.')
+    32
+
+    Explanation:
+    The regular expression '\s+' matches one or more whitespace characters including space, tab, newline, carriage return, etc.
+    The 're.sub' function replaces all matching substrings with an empty string thereby removing them from the input string.
+    The length of the resulting string gives us the count of non-whitespace characters in the input string.
+    """
     return utils._get_char_count(text)
 
-def get_avg_wordlength(text):
+def get_avg_wordlength(text: str) -> float:
+    """
+    Returns the average length of words in a given text.
+
+    Parameters:
+    - text (str): The input string from which to calculate the average word length.
+
+    Returns:
+    - float: The average length of words in the input text.
+
+    Example:
+    >>> text = "The quick brown fox jumps over the lazy dog"
+    >>> get_avg_wordlength(text)
+    3.5
+    """
     return utils._get_avg_wordlength(text)
 
-def get_stopwords_count(text):
+def get_stopwords_count(text: str) -> int:
+    """
+    Calculates the count of stopwords in the given text.
+
+    Parameters:
+        - text (str): A string of text to analyze for stopwords.
+
+    Returns:
+        - int: The count of stopwords found in the text.
+
+    Example usage:
+    >>> text = "The quick brown fox jumps over the lazy dog"
+    >>> get_stopwords_count(text)
+    4
+    """
     return utils._get_stopwords_count(text)
 
-def get_hashtag_count(text):
+def get_hashtag_count(text: str) -> int:
+    """
+    Returns the number of words that start with '#' in the given text.
+    
+    Parameters:
+    - text (str): The text to search for hashtags.
+    
+    Returns:
+    - int: The number of words that start with '#'.
+    """
     return utils._get_hashtag_count(text)
 
 def get_mentions_count(text):
+    """
+     Returns the number of words that start with '@' in the given text.
+    
+    Parameters:
+    - text (str): The text to search for mentions.
+    
+    Returns:
+     -int: The number of words that start with '@'.
+    """
     return utils._get_mentions_count(text)
 
 def get_digit_count(text):
